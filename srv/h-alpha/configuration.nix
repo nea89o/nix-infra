fd34381a09d0691ad90a374e1ce639d591d253f6
Linnea Gräf <nea@nea.moe>
2024-12-13 00:34:53 +0100

    Update

--- srv/h-alpha/configuration.nix
+++ srv/h-alpha/configuration.nix
@@ -17,6 +17,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINg2WYMRKINwbH5UCqqK2qq/qW0gG1NnaALHqEyU4NzM nea@smiley-mk1"
     ];
+    initialHashedPassword = "$y$j9T$a4xR89vrsf1Kp1Rm.hxBM.$0KMdqEwi1uU6AbXKr.cdbQXahjs2TNairGr/3PMO6s7";
   };
 
   security.sudo.wheelNeedsPassword = false;
@@ -25,10 +26,12 @@
     enable = true;
     settings = {
       PermitRootLogin = "no";
-      PasswordAuthentication = false;
+      #      PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
     };
   };
+
+  networking.hostName = "alpha-site";
 
   system.stateVersion = "24.11";
 }
