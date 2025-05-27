9dd529ff04dcf837ff03be259b9709e0e57956ca
Linnea Gräf <nea@nea.moe>
2025-05-27 10:32:01 +0200

    snapshot: Di 27. Mai 10:32:01 CEST 2025

--- srv/hadante/fuzzel.nix
+++ srv/hadante/fuzzel.nix
@@ -3,6 +3,11 @@
   home-manager.users.${config.user} = {
     programs.fuzzel.enable = true;
     programs.fuzzel.settings = {
+      main = {
+        font = "ComicShannsMono Nerd Font";
+        use-bold = true;
+        fields = "name,categories";
+      };
     };
   };
 }
