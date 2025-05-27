dc4894d422cdd3b98f5344586cafd812f9a734c7
Linnea Gräf <nea@nea.moe>
2025-05-27 10:58:51 +0200

    snapshot: Di 27. Mai 10:58:51 CEST 2025

--- srv/hadante/fuzzel.nix
+++ srv/hadante/fuzzel.nix
@@ -1,13 +1,26 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+let
+
+  palette = inputs.catppuccin.legacyPackages.x86_64-linux.palette;
+  colors = builtins.fromJSON (builtins.readFile "${palette.out}/palette.json");
+  mocha = colors.mocha.colors;
+in
 {
   home-manager.users.${config.user} = {
     programs.fuzzel.enable = true;
-    catppuccin.fuzzel.enable = true;
     programs.fuzzel.settings = {
       main = {
         font = "ComicShannsMono Nerd Font";
         use-bold = true;
         fields = "name,categories";
+      };
+      colors = {
+        background = mocha.surface0;
       };
     };
   };
