d9a8f843e2c190618c2a30141e9c00251b0040d8
Linnea Gräf <nea@nea.moe>
2025-05-29 19:09:24 +0200

    snapshot: Do 29. Mai 19:09:24 CEST 2025

--- srv/hadante/vintage-story.nix
+++ srv/hadante/vintage-story.nix
@@ -10,17 +10,16 @@ let
 in
 {
   home-manager.users.${config.user} = {
-    imports = [ inputs.vintagestory-nix.homeManagerModules.default ];
-    programs.vs-launcher = {
-      enable = true;
-      installedVersions = with VSPkgs; [
-        # Current version I'm playing on with mods
-        v1-20-10
-      ];
-    };
+    # imports = [ inputs.vintagestory-nix.homeManagerModules.default ];
+    # programs.vs-launcher = {
+    #   enable = true;
+    #   installedVersions = with VSPkgs; [
+    #     # Current version I'm playing on with mods
+    #     v1-20-10
+    #   ];
+    # };
     home.packages = [
-      VSPkgs.latest
-      pkgs.dotnet-runtime
+      pkgs.vintagestory
     ];
   };
 }
