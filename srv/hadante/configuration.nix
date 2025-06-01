d8bfd17501eec387352f019814ffc478d1cd2ee4
Linnea Gräf <nea@nea.moe>
2025-06-01 17:31:06 +0200

    snapshot: So 1. Jun 17:31:06 CEST 2025

--- srv/hadante/configuration.nix
+++ srv/hadante/configuration.nix
@@ -205,6 +205,8 @@ in
         let
           javas = {
             jdk21 = pkgs.jdk21;
+            jdk22 = pkgs.jdk22;
+            jdk24 = pkgs.jdk24;
             jdk17 = pkgs.jdk17;
             jdk11 = pkgs.jdk11;
             jdk8 = pkgs.jdk8;
